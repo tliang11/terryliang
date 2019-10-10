e68d14a8bc1aa5edaeef723cea471b3ce575b68d
_____Terry Liang____ <_____Terry Liang____@DESKTOP-LB6FKPP>
2019-10-09 22:14:35 -0700

    last version.

--- 2019Fall/src/cse360assign2/AddingMachine.java
+++ 2019Fall/src/cse360assign2/AddingMachine.java
@@ -1,8 +1,9 @@
-//*  Author: Tianyi Liang
+// * Author: Tianyi Liang
 // * Asu ID: 1213381791
 // * Assignment: assignment 2
 // * Date: 2019/10/09
-// * Description: 
+// * Description: learn to use version a control system. add sub values. 
+//https://github.com/tliang11/terryliang/tree/master/2019Fall/src/cse360assign2
  
 
 
@@ -14,28 +15,34 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	
+	private StringBuffer data = new StringBuffer("0");//use string buffer to save values.
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;//return totaal
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		data.append("+");//use append to save add values.
+		data.append(value);
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		data.append("-");////use append to save sub values.
+		data.append(value);
 	}
 		
 	public String toString () {
-		return "";
+		return data.toString();
 	}
 
 	public void clear() {
-	
+	    total = 0;
+	    data.delete(0, data.length());//delete all values.
 	}
 }
---
+++ 2019Fall/src/cse360assign2/Test.java
@@ -0,0 +1,14 @@
+package cse360assign2;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		AddingMachine test = new AddingMachine();
+		 test.add(4);
+		 test.subtract (2);
+		 test.add(5);
+		 
+		 System.out.println(test.toString());
+	}
+}
